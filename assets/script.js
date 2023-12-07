e2c1e86c30742759f9a7f5472ad0816a3cc9c841
ulisesd404 <144632800+ulisesd404@users.noreply.github.com>
2023-12-06 23:22:50 -0800

    everything works properly, comments still needed and readme has not been started

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,30 @@
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    $('.saveBtn').on('click', function(){
-        var input = $(this).siblings('.description').val().trim();
-        var timeId = $(this).parent().attr('id');
-        if (input === ''){
-            return;
-        } else {
-            localStorage.setItem(timeId,input);
-            displayInput();
+//saves input onto local storage when button is pressed
+$('.saveBtn').on('click', function(){
+    var input = $(this).siblings('.description').val().trim();
+    var timeId = $(this).parent().attr('id');
 
-        };
-        
-    });
-    
-    function displayInput() {
-        var timeId = $('.time-block').siblings().attr('id');
-        var tasks = localStorage.getItem(timeId);
-        var textArea = $(timeId).child('.description').val();
-        var containers = $('.time-block');
-        
-
-        containers.forEach(function(){
-            textArea.textContent = tasks;
-        })
-            
-        };
-
-    
+    if (input === ''){
+        return;
+    } else {
+        localStorage.setItem(timeId,input);
+        displayInput();
+    }
+})
         
     
-
+//gets local storage items and places it on html
+    $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+    $('#hour-1 .description').val(localStorage.getItem('hour-1'));
+    $('#hour-2 .description').val(localStorage.getItem('hour-2'));
+    $('#hour-3 .description').val(localStorage.getItem('hour-3'));
+    $('#hour-4 .description').val(localStorage.getItem('hour-4'));
+    $('#hour-5 .description').val(localStorage.getItem('hour-5'));    
     
-
-    //changes background of the blocks depending on the time
+//changes background of the blocks depending on the time
     function addBackgroundColor() {
         var currentHour = dayjs().hour();
     
@@ -73,7 +60,6 @@ $('#currentDay').text(currentDay);
 
 
 addBackgroundColor();
-displayTaks();
 });
 
   
